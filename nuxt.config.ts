e9a1418fbe37c852ba2c17a25353508257040550
Qujia <duqujia@hotmail.com>
2024-10-10 18:10:08 +0800

    feat: Add Algolia , i18n, and Strapi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+    '@nuxtjs/strapi',
+    '@nuxtjs/algolia'
+  ],
+  strapi: {
+    // Options
+      url: process.env.STRAPI_URL || 'http://localhost:1337',
+      prefix: '/api',
+      admin: '/admin',
+      version: 'v4',
+      cookie: {},
+      cookieName: 'strapi_jwt'
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <!-- <h1>Nuxt Routing set up successfully!</h1>
+    <p>Current route: {{ route.path }}</p>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a> -->
+    <h1 class="text-2xl font-bold text-center mt-10"> Micen's Website </h1>
+  </div>
+</template>

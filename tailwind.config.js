b742ea40fc2460c23b6842c7f6f9dd13d986d848
Micenjia <duqujia@hotmail.com>
2024-10-25 23:33:14 +0800

    feat(config): Added Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                "background": "rgba(251, 251, 254, 1)",
+                "glass-white-2": "rgba(255, 255, 255, 0.2)",
+                "glass-white-4": "rgba(255, 255, 255, 0.4)",
+                "primary-blue": "rgba(39, 96, 241, 1)",
+                "light-green": "rgba(204, 243, 129, 1)",
+                "light-pink": rgba(238, 174, 202, 1),
+            }
+        },
+    },
+    plugins: [],
+}

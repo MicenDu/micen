0f10810f5c2ec7d629750c4ebe5bb5e27d24722d
Micenjia <duqujia@hotmail.com>
2024-10-26 23:18:33 +0800

    feat: Add responsive footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,38 @@
+<template>
+    <footer>
+        <div
+            class="flex flex-row justify-between max-w-full absolute inset-x-0 bottom-0 bg-glass-white-2 py-8 px-8 md:px-36 md:mx-16">
+            <div class="flex text-sm text-gray-500 flex-col gap-4 md:flex-row md:gap-8">
+                <span class="font-medium text-gray-600">Micen © 2024</span>
+                <div class="flex gap-6 ">
+                    <a href="">首页</a>
+                    <a href="">项目</a>
+                    <a href="">生活</a>
+                    <a href="">关于</a>
+                </div>
+            </div>
+            <div class="gap-4 grid grid-cols-2 md:grid-cols-4 ">
+                <a href="">
+                    <img src="../assets/icon/Github.svg" alt="">
+                </a>
+                <a href="">
+                    <img src="../assets/icon/LinkedIn.svg" alt="">
+                </a>
+                <a href="">
+                    <img src="../assets/icon/Mail.svg" alt="">
+                </a>
+                <a href="">
+                    <img src="../assets/icon/Instagram.svg" alt="">
+                </a>
+            </div>
+        </div>
+
+
+    </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>

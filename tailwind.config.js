c63c5bf21a6201e6fa837b58830269be5bde0579
Micenjia <duqujia@hotmail.com>
2024-10-27 23:00:08 +0800

    feat(config): Update Tailwind color configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
-const colors = require("tailwindcss/colors");
+// const colors = require("tailwindcss/colors");
 module.exports = {
-    content: [],
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+    ],
     theme: {
         extend: {
             colors: {
-                "background": "rgba(251, 251, 254, 1)",
-                "glass-white-2": "rgba(255, 255, 255, 0.2)",
-                "glass-white-4": "rgba(255, 255, 255, 0.4)",
-                "primary-blue": "rgba(39, 96, 241, 1)",
-                "light-green": "rgba(204, 243, 129, 1)",
-                "light-pink": rgba(238, 174, 202, 1),
+                'glass-white-2': 'rgba(255, 255, 255, 0.2)',
+                'glass-white-4': 'rgba(255, 255, 255, 0.4)',
+                'primary-blue': 'rgba(39, 96, 241, 1)',
+                'light-green': 'rgba(204, 243, 129, 1)',
+                'light-pink': 'rgba(238, 174, 202, 1)',
             }
         },
     },

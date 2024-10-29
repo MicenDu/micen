6ab666531b9790228952ac00d0150dd194f800aa
Qujia <duqujia@hotmail.com>
2024-10-29 10:11:42 +0800

    feat: Add responsive project card

---
+++ components/ProjectCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <article class=" bg-glass-white-2 backdrop-blur-md shadow-sm mx-auto md:w-3/4">
+    <a href="#" class="grid md:grid-cols-10 grid-reverse md:max-h-96 overflow-hidden">
+      <div class="p-8 bg-glass-white-4 backdrop-blur-md flex-col justify-between hidden col-span-4 max-h-96 md:flex ">
+        <div>
+          <div class="flex justify-between items-center text-gray-400 text-sm my-4">
+            <p>2023</p>
+            <p>中国、非洲</p>
+          </div>
+          <div>
+            <h2 class="uppercase text-gray-800 mb-2 text-xl font-bold">h2 title</h2>
+            <p class="text-gray-500 mb-2 leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+              quasi, ratione
+              necessitatibus possimus </p>
+          </div>
+        </div>
+        <div>
+          <p class="text-gray-400 text-sm">关于项目</p>
+        </div>
+      </div>
+      <div class="h-auto overflow-hidden shrink-0 md:col-span-6">
+        <img src="../assets/card-cover-1.webp" alt="">
+      </div>
+      <div class="p-4 bg-glass-white-4 backdrop-blur-md md:hidden">
+        <div>
+          <h2 class="uppercase text-gray-800 mb-2 text-xl font-bold">h2 title</h2>
+          <p class="text-gray-500 mb-4 leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            quasi, ratione
+            necessitatibus possimus </p>
+        </div>
+        <div class="flex justify-between items-center text-gray-400 text-sm my-2">
+          <p>2023</p>
+          <p>中国、非洲</p>
+        </div>
+        <div>
+          <p class="text-gray-400 text-sm hidden md:block">关于项目</p>
+        </div>
+      </div>
+
+    </a>
+  </article>
+
+</template>
+
+<script setup>
+
+
+</script>

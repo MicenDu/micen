9fbabf4cc6ac9fc01dc9727289f5737ba3f47604
Micenjia <duqujia@hotmail.com>
2024-10-29 23:16:18 +0800

    feat: Add responsive quote card

---
+++ components/QuoteCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div
+        class=" p-4 bg-glass-white-4 backdrop-blur-md border-glass-white-4 border-2 shadow-sm flex flex-col justify-center gap-2 min-h-52 md:max-w-3xl md:h-80 md:p-8">
+        <blockquote cite="#" class="">
+            <p class="text-4xl font-bold text-zinc-700 line-clamp-3 mb-3"> Lorem ipsum dolor sit amet
+                consectetur.
+            </p>
+        </blockquote>
+        <div class="flex justify-end">
+            <cite class="text-zinc-400">
+                —— sit amet (2024)
+            </cite>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>

e61db8c6c70ba8610a01278642c6a0609ff218b7
Micenjia <duqujia@hotmail.com>
2024-10-31 22:51:28 +0800

    feat: Add categories component for the article page

---
+++ components/Categories.vue
@@ -0,0 +1,8 @@
+<template>
+    <a href="#"
+        class="text-gray-500 bg-gray-50 border-2 border-glass-white-2 rounded-full px-3 py-1 m-2 capitalize font-medium"
+        @click.prevent>品牌</a>
+</template>
+
+<script setup>
+</script>

faff94a28712e547437ef89beffc57517a4a691a
Micenjia <duqujia@hotmail.com>
2024-10-31 22:43:15 +0800

    feat: Add responsive blog card

---
+++ components/BlogCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <article>
+        <div class="bg-glass-white-4 min-w-96 min-h-72 mx-auto md:max-w-xl md:max-h-xl">
+            <img src="../assets/images/article.png" alt="" class="object-cover object-center h-full w-full">
+            <div class=" border-glass-white-2 border-2">
+                <a href="">
+                    <div class="flex justify-between p-4">
+                        <h2 class="text-gray-600">这是一篇文章</h2>
+                        <time datetime="2022-01-22" class="text-gray-400">2022/01/22</time>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </article>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>

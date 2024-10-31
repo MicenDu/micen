a5c52b49882e93d22f6da03d16e21475627f2ac7
Micenjia <duqujia@hotmail.com>
2024-10-31 22:46:48 +0800

    feat: Added reel component to display recently watched movies and books.

---
+++ components/Reel.vue
@@ -0,0 +1,40 @@
+<!-- <template>
+    <article class="bg-glass-white-4 max-w-96 max-h-80 flex flex-row mx-auto">
+        <div class="shrink border-2 border-glass-white-2 p-6 grow w-full backdrop-blur-md">
+            <a href="" class="text-primary-blue flex flex-col justify-between gap-4 h-full">
+                <div>
+                    <h2 class="mb-2">黑客与画家</h2>
+                    <p>保罗·格雷厄姆</p>
+                </div>
+                <time datetime="">2022</time>
+            </a>
+        </div>
+        <div>
+            <img src="../assets/images/hackers.png" alt="" class="object-cover object-center h-full w-full">
+        </div>
+    </article>
+</template> -->
+<template>
+    <article class="bg-glass-white-4 max-w-96 max-h-80 flex flex-col mx-auto">
+        <!-- flex-row -->
+        <div class="shrink border-2 border-glass-white-2 p-6 grow w-full backdrop-blur-md">
+            <a href="" class="text-primary-blue flex flex-row justify-between gap-4 h-full">
+                <!-- flex-col -->
+                <div>
+                    <h2 class="mb-2">周处除三害</h2>
+                    <p>黄精甫</p>
+                </div>
+                <time datetime="">2023</time>
+            </a>
+        </div>
+        <div>
+            <img src="../assets/images/the_pig.png" alt="" class="object-cover object-center h-full w-full">
+        </div>
+    </article>
+</template>
+
+<script setup>
+
+
+
+</script>

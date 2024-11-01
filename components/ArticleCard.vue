a9c93a288b398e3b05e54e4afa5956d5da55a421
Micenjia <duqujia@hotmail.com>
2024-11-01 23:35:21 +0800

    chore(icons): Rename icon folder to icons

---
+++ components/ArticleCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <article class="bg-gray-200 w-96 h-44 isolate relative mx-auto md:h-72 md:w-5/12">
+        <img src="../assets/images/article.png" alt=""
+            class="absolute object-cover object-left h-full w-full inset-0 -z-20">
+        <div class="p-4 z-10 h-full">
+            <a href="" class="flex flex-col justify-between text-white h-full z-10 w-fit text-shadow">
+                <time datetime="2022-01-22">2022/01/22</time>
+                <h2>我的第一篇文章</h2>
+            </a>
+        </div>
+
+    </article>
+</template>
+
+<script setup>
+
+</script>

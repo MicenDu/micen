188c572e5fea61c07521a756d4ae51bae532d496
Micenjia <duqujia@hotmail.com>
2024-11-01 23:15:21 +0800

    feat: Add responsive client card

---
+++ components/ClientCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex flex-wrap w-80 justify-between gap-4 mx-auto items-start p-4 glass-border md:flex-col">
+        <div class="flex flex-col justify-center ml-4 gap-0.5 w-fit">
+            <p class="font-medium text-md text-neutral-700">客户</p>
+            <div class="flex flex-col gap-0.5">
+                <p class="text-sm text-neutral-500">德诚控股集团</p>
+            </div>
+        </div>
+        <div class="flex flex-col justify-center ml-4 gap-1 w-fit">
+            <p class="font-medium text-md text-neutral-700">类型</p>
+            <div class="flex flex-col gap-0.5">
+                <p class="text-sm text-neutral-500">UI/UX</p>
+                <p class="text-sm text-neutral-500">前端</p>
+            </div>
+
+        </div>
+        <div class="flex flex-col justify-center ml-4 gap-1 w-full">
+            <p class="font-medium text-md text-neutral-700">工具方法</p>
+            <div class="flex flex-col gap-0.5">
+                <p class="text-sm text-neutral-500">标杆研究</p>
+                <p class="text-sm text-neutral-500">低保真分析</p>
+                <p class="text-sm text-neutral-500">Bootstrap</p>
+                <p class="text-sm text-neutral-500">Midjourney</p>
+                <p class="text-sm text-neutral-500">Google Analysis</p>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>

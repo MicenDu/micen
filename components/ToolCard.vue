d0951add2f83d2db71ed782ef5a2335e725c4355
Micenjia <duqujia@hotmail.com>
2024-11-01 23:14:37 +0800

    feat: Add tool card

---
+++ components/ToolCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-52 h-20 p-4 hover:bg-glass-white-4 hover:border-2 hover:border-glass-white-2">
+        <a href="" class="flex flex-row items-center justify-center h-full">
+            <img src="../assets/images/tools/photoshop.png" alt="">
+            <div class="flex flex-col justify-center ml-4 gap-0.5">
+                <p class="font-medium text-md text-neutral-700">Photoshop</p>
+                <p class="text-sm text-neutral-500">图片处理</p>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script setup>
+
+</script>

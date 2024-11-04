26e60f753244157b927b57c53dd988e694c20ece
Micenjia <duqujia@hotmail.com>
2024-11-04 23:19:05 +0800

    feat: Added button component

---
+++ components/GlassButton.vue
@@ -0,0 +1,10 @@
+<template>
+    <button
+        class="font-bold text-primary-blue px-4 py-2 rounded-full glass-border hover:bg-white/30 transition-all duration-300 w-fit">
+        <slot />
+    </button>
+</template>
+
+<script setup>
+
+</script>

6e0d21acaf4f68df930b19822a882a924aeae9e6
Micenjia <duqujia@hotmail.com>
2024-11-09 22:59:28 +0800

    feat: Unify responsive breakpoints for hero section and footer

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="h-96 flex justify-center w-full md:h-[60vh]">
+    <div class="h-96 flex justify-center w-full md:h-[75vh]">
         <NuxtImg src="/images/bg_gradient_pc.png" alt="" loading="lazy"
-            class="absolute object-cover w-full inset-0 z-10 h-96 md:h-4/6 " format="avif,webp" />
+            class="absolute object-cover w-full inset-0 z-10 h-96 md:h-[75vh] " format="avif,webp" />
 
         <!-- <iframe src='https://my.spline.design/herosection-1d552219c8fbb5a64afbf85567f8d549/' frameborder='0'
             width='100%' height='100%' class=" absolute object-cover w-full inset-0  "></iframe> -->
         <div class="flex container flex-col justify-center items-start w-full m-8 gap-4 z-10 md:pl-20 ">
             <h1
-                class="text-2xl font-bold text-primary-blue leading-normal w-fit sm:text-3xl md:text-5xl md:leading-normal">
+                class="text-2xl font-bold  leading-normal w-fit sm:text-3xl md:text-5xl md:leading-normal bg-gradient-to-r from-primary-blue to-light-green bg-clip-text text-transparent">
                 你好哇！这是Micen
-                <br> 他是一名<span>设计师</span>
+                <br> 一名<span>设计师</span>
             </h1>
             <GlassButton>Say Hi~ o(*￣▽￣*)ブ</GlassButton>
         </div>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,28 +1,27 @@
 <template>
-    <footer>
-        <div
-            class="flex flex-row justify-between max-w-full absolute inset-x-0 bottom-0 bg-glass-white-2 py-8 px-8 md:px-36 md:mx-16">
+    <footer class="glass bottom-0 w-full mt-20">
+        <div class="flex flex-row container justify-between relative py-6 px-8 md:mx-auto">
             <div class="flex text-sm text-gray-500 flex-col gap-4 md:flex-row md:gap-8">
                 <span class="font-medium text-gray-600">Micen © 2024</span>
                 <div class="flex gap-6 ">
-                    <a href="">首页</a>
-                    <a href="">项目</a>
-                    <a href="">生活</a>
-                    <a href="">关于</a>
+                    <a href="/">首页</a>
+                    <a href="/projects">项目</a>
+                    <a href="/life">生活</a>
+                    <a href="/about">关于</a>
                 </div>
             </div>
             <div class="gap-4 grid grid-cols-2 md:grid-cols-4 ">
                 <a href="">
-                    <img src="../assets/icon/Github.svg" alt="">
+                    <img src="../assets/icons/Github.svg" alt="">
                 </a>
                 <a href="">
-                    <img src="../assets/icon/LinkedIn.svg" alt="">
+                    <img src="../assets/icons/LinkedIn.svg" alt="">
                 </a>
                 <a href="">
-                    <img src="../assets/icon/Mail.svg" alt="">
+                    <img src="../assets/icons/Mail.svg" alt="">
                 </a>
                 <a href="">
-                    <img src="../assets/icon/Instagram.svg" alt="">
+                    <img src="../assets/icons/Instagram.svg" alt="">
                 </a>
             </div>
         </div>

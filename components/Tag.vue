ea319b0473c2815a628bd68bfb3d7f4f1a716822
Qujia <duqujia@hotmail.com>
2024-11-13 10:41:17 +0800

    fix: Rename Categories.vue component to Tag.vue

---
+++ components/Tag.vue
@@ -0,0 +1,10 @@
+<template>
+    <a href="#"
+        class="text-zinc-500 bg-gray-50 border-2 border-glass-white-2 rounded-full px-2 py-0 capitalize font-medium"
+        @click.prevent>
+        <slot></slot>
+    </a>
+</template>
+
+<script setup>
+</script>

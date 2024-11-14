566a3fe4df773e0253d96637bffb811b5f16358c
Qujia <duqujia@hotmail.com>
2024-11-14 16:25:00 +0800

    feat: Use slots for QuoteCard

--- components/QuoteCard.vue
+++ components/QuoteCard.vue
@@ -1,14 +1,18 @@
 <template>
-    <div
-        class=" p-4 bg-glass-white-4 backdrop-blur-md border-glass-white-4 border-2 shadow-sm flex flex-col justify-center gap-2 min-h-52 md:max-w-3xl md:h-80 md:p-8">
-        <blockquote cite="#" class="">
-            <p class="text-4xl font-bold text-zinc-700 line-clamp-3 mb-3"> Lorem ipsum dolor sit amet
-                consectetur.
+    <div class="p-4 my-4 glass-border flex flex-col justify-center gap-2 flex-shrink-0 
+                min-h-44 w-[90vw] max-w-3xl mx-auto
+                md:min-h-52 md:h-80 md:p-8">
+        <blockquote cite="#" class="mt-8">
+            <p class="text-3xl font-bold text-zinc-700 line-clamp-3 mb-3 w-full text-center font-serif md:text-4xl">
+                “ <slot name="quote"></slot> ”
             </p>
         </blockquote>
         <div class="flex justify-end">
-            <cite class="text-zinc-400">
-                —— sit amet (2024)
+            <cite class="text-zinc-400 font-serif">
+                —— <slot name="cite"></slot>
+                <span class="font-sans">
+                    (<slot name="year"></slot>)
+                </span>
             </cite>
         </div>
     </div>

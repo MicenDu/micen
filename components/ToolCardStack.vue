328e2b5782d73353da89fea43c0657a1839e57e1
Qujia <duqujia@hotmail.com>
2024-11-14 16:28:18 +0800

    feat: Added ToolCardStack

---
+++ components/ToolCardStack.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="pb-2">
+        <p class="text-zinc-600 text-sm mt-4 mb-4 ">
+            <slot name="category"></slot>
+        </p>
+        <div class="grid grid-cols-2 gap-6 md:grid-cols-3">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>

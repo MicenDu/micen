626f0668766b2ea5ccd02e00302206a4244919e6
Micenjia <duqujia@hotmail.com>
2024-11-17 22:29:59 +0800

    feat: Added slots, props and NuxtImg for ProjectCard

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,39 +1,78 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: String,
+    required: true
+  },
+  region: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  imgUrl: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
 <template>
-  <article class=" bg-glass-white-2 backdrop-blur-md shadow-sm mx-auto md:w-3/4">
-    <a href="#" class="grid md:grid-cols-10 grid-reverse md:max-h-96 overflow-hidden">
-      <div class="p-8 bg-glass-white-4 backdrop-blur-md flex-col justify-between hidden col-span-4 max-h-96 md:flex ">
+  <article class="mx-auto my-4 md:my-8 lg:w-full">
+    <a href="#" class="grid lg:grid-cols-10 grid-reverse max-h-96 overflow-hidden">
+      <div class="p-8 glass-border flex-col justify-between hidden col-span-4 max-h-96 lg:flex ">
         <div>
           <div class="flex justify-between items-center text-gray-400 text-sm my-4">
-            <p>2023</p>
-            <p>中国、非洲</p>
+            <time datetime="2023">
+              <slot name="year"></slot>
+            </time>
+            <p>
+              <slot name="region"></slot>
+            </p>
           </div>
           <div>
-            <h2 class="uppercase text-gray-800 mb-2 text-xl font-bold">h2 title</h2>
-            <p class="text-gray-500 mb-2 leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              quasi, ratione
-              necessitatibus possimus </p>
+            <h2 class="uppercase text-gray-800 mb-2">
+              <slot name="title"></slot>
+            </h2>
+            <p class="text-gray-500 mb-2 leading-6">
+              <slot name="description"></slot>
+            </p>
           </div>
         </div>
         <div>
           <p class="text-gray-400 text-sm">关于项目</p>
         </div>
       </div>
-      <div class="h-auto overflow-hidden shrink-0 md:col-span-6">
-        <img src="../assets/card-cover-1.webp" alt="">
+      <div class="max-h-96 overflow-hidden shrink-0 lg:col-span-6 relative ">
+        <NuxtImg :src="imgUrl" alt="" class="w-full h-full object-cover object-center" loading="lazy" placeholder
+          format="avif,webp" />
       </div>
-      <div class="p-4 bg-glass-white-4 backdrop-blur-md md:hidden">
-        <div>
-          <h2 class="uppercase text-gray-800 mb-2 text-xl font-bold">h2 title</h2>
-          <p class="text-gray-500 mb-4 leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            quasi, ratione
-            necessitatibus possimus </p>
+      <div class="p-4 glass-border col-auto lg:hidden">
+        <div class="text-sm">
+          <h2 class=" text-gray-800 mb-2 font-bold text-sm">
+            <slot name="title"></slot>
+          </h2>
+          <p class="text-gray-500 mb-2 leading-6">
+            <slot name="description"></slot>
+          </p>
         </div>
-        <div class="flex justify-between items-center text-gray-400 text-sm my-2">
-          <p>2023</p>
-          <p>中国、非洲</p>
-        </div>
-        <div>
-          <p class="text-gray-400 text-sm hidden md:block">关于项目</p>
+        <div class="flex justify-between items-center text-gray-400 text-xs my-2">
+          <p>
+            <slot name="year"></slot>
+          </p>
+          <p>
+            <slot name="region"></slot>
+          </p>
         </div>
       </div>
 
@@ -41,8 +80,3 @@
   </article>
 
 </template>
-
-<script setup>
-
-
-</script>

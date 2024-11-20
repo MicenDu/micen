06a458357e98489e1ba8bdc84263f02fb183b442
Micenjia <duqujia@hotmail.com>
2024-11-20 22:44:28 +0800

    feat: Change text color and margin of Tag

--- components/Tag.vue
+++ components/Tag.vue
@@ -1,8 +1,8 @@
 <template>
     <a href="#"
-        class="text-zinc-500 bg-gray-50 border-2 border-glass-white-2 rounded-full px-2 py-0 capitalize font-medium"
+        class="bg-zinc-100 text-sm w-fit text-primary-blue/75 border-2 border-glass-white-2 rounded-full px-2.5 py-1 mb-1 font-medium"
         @click.prevent>
-        <slot></slot>
+        <slot />
     </a>
 </template>
 

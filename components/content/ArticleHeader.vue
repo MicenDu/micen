207c70f7893593928824341948aed9cf8973d545
Micenjia <duqujia@hotmail.com>
2024-11-24 23:58:40 +0800

    feat: Added ArticleHeader

---
+++ components/content/ArticleHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex justify-center">
+        <!-- <a href="#"
+            class="text-primary-blue/80 bg-blue-100 hover:bg-blue-200 border-2 border-white/20 rounded-full px-2.5 py-0 mx-1 text-sm font-normal text-center w-fit "
+            @click.prevent> -->
+
+        <slot></slot>
+        <!-- </a> -->
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    tags: {
+        type: Array,
+        required: true
+    }
+})
+</script>

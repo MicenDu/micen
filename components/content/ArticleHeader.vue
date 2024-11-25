e710f169f39bff7f3d46c6dac3713c4acca657ef
Qujia <duqujia@hotmail.com>
2024-11-25 16:22:10 +0800

    refactor: Replace slot with data binding of ArticleHeader and ClientCard

--- components/content/ArticleHeader.vue
+++ components/content/ArticleHeader.vue
@@ -1,18 +1,47 @@
 <template>
     <div class="flex justify-center">
-        <!-- <a href="#"
-            class="text-primary-blue/80 bg-blue-100 hover:bg-blue-200 border-2 border-white/20 rounded-full px-2.5 py-0 mx-1 text-sm font-normal text-center w-fit "
-            @click.prevent> -->
-
-        <slot></slot>
-        <!-- </a> -->
+        <div class="flex flex-col gap-3 items-center">
+            <div class="text-center">
+                <p class="text-primary-blue text-2xl md:text-4xl font-medium mb-5 mt-3">
+                    {{ title }}
+                </p>
+                <time class="text-primary-blue/80 my-3 font-sans;">
+                    {{ date }}
+                </time>
+            </div>
+            <div>
+                <a v-for="tag in tags" :key="tag"
+                    class="text-primary-blue/80 bg-blue-100 hover:bg-blue-200 border-2 border-white/20 rounded-full px-2.5 py-0 mx-1 text-sm font-normal text-center w-fit cursor-default">
+                    {{ tag }}
+                </a>
+            </div>
+            <img :src="image" :alt="alt" class="mt-3">
+        </div>
     </div>
 </template>
 
 <script setup>
 defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
     tags: {
         type: Array,
+        required: true
+    },
+
+    image: {
+        type: String,
+        required: true
+    },
+
+    alt: {
+        type: String,
         required: true
     }
 })
--- components/content/ClientCard.vue
+++ components/content/ClientCard.vue
@@ -3,25 +3,25 @@
         <div class="flex flex-col justify-center px-4 gap-1 w-fit">
             <p class="font-medium text-base text-neutral-600 ">客户</p>
             <div class="gap-1">
-                <p class="text-sm text-neutral-500">
-                    <slot name="company"></slot>
+                <p class=" text-neutral-500">
+                    {{ company }}
                 </p>
             </div>
         </div>
         <div class="flex flex-col justify-center px-4 gap-1 w-fit">
             <p class="font-medium text-base text-neutral-600 ">类型</p>
             <div class="flex flex-row gap-1 md:flex-col">
-                <!-- <p class="text-sm text-neutral-500"> -->
-                <slot name="types"></slot>
-                <!-- </p> -->
+                <p v-for="type in types" :key="type" class="text-sm text-neutral-500">
+                    {{ type }}
+                </p>
             </div>
         </div>
         <div class="flex flex-col justify-center px-4 gap-1 w-full">
             <p class="font-medium text-base text-neutral-600 ">工具方法</p>
             <div class="gap-1">
-                <!-- <p class="text-sm text-neutral-500"> -->
-                <slot name="tools"></slot>
-                <!-- </p> -->
+                <p v-for="tool in tools" :key="tool" class="text-sm text-neutral-500">
+                    {{ tool }}
+                </p>
             </div>
 
         </div>
@@ -32,12 +32,15 @@
 defineProps({
     company: {
         type: String,
+        required: true,
     },
     types: {
         type: Array,
+        required: true,
     },
     tools: {
         type: Array,
+        required: true,
     }
 })
 </script>

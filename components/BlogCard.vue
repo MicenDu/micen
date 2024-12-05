6ebc21d9ffbf74666ebac3bf4c00da364bffc770
Qujia <duqujia@hotmail.com>
2024-12-05 17:41:30 +0800

    refactor: Replace slot with interpolate

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -2,16 +2,15 @@
     <article>
         <div class="">
             <a :href="link">
-                <NuxtImg :src="imgUrl" alt="" format="avif, webp"
+                <NuxtImg :src="imgUrl" :alt="alt" format="avif, webp"
                     class="object-cover object-center h-40 xl:h-64 w-full box-border" />
                 <div class="glass-border w-full">
-
                     <div class="flex justify-between p-4">
                         <h2 class="text-zinc-600">
-                            <slot name="title"></slot>
+                            {{ title }}
                         </h2>
-                        <time datetime="2022-01-22" class="text-gray-400">
-                            <slot name="date"></slot>
+                        <time :datetime="date" class="text-gray-400">
+                            {{ date }}
                         </time>
                     </div>
 
@@ -35,9 +34,14 @@ defineProps({
         type: String,
         required: true
     },
+    alt: {
+        type: String,
+        required: true
+    },
     link: {
         type: String,
         required: true
-    }
+    },
+
 })
 </script>

48b61f028bebbfec56d091eb286ee3888d06d0e5
Micenjia <duqujia@hotmail.com>
2024-12-11 21:28:39 +0800

    feat: Added chineseNumber list

--- components/GlassCard.vue
+++ components/GlassCard.vue
@@ -1,15 +1,17 @@
 <template>
     <article class="glass-border px-6 pt-6 pb-8 my-4  md:my-8 md:px-28 md:py-12">
-        <div class="flex flex-col md:flex-row gap-3 md:gap-24 items-start md:items-center">
-            <p class="text-2xl md:text-7xl font-serif font-bold text-zinc-600">{{ index }}</p>
-            <div class="flex flex-col gap-4 w-full">
-                <h2 class="text-lg md:text-2xl font-serif text-zinc-600 font-bold">{{ title }}</h2>
-                <p class="font-serif text-lg text-zinc-500">{{ description }}</p>
-                <p class="font-mono w-full text-end text-sm text-zinc-500 hidden md:block">
-                    <time :datetime="date">{{ date }}</time>
-                </p>
+        <nuxt-link :to="link">
+            <div class="flex flex-col md:flex-row gap-3 md:gap-24 items-start md:items-center">
+                <p class="text-2xl md:text-7xl font-serif font-bold text-zinc-600">{{ chineseNumbers(index) }}</p>
+                <div class="flex flex-col gap-4 w-full">
+                    <h2 class="text-lg md:text-2xl font-serif text-zinc-600 font-bold">{{ title }}</h2>
+                    <p class="font-serif text-lg text-zinc-500">{{ description }}</p>
+                    <p class="font-mono w-full text-end text-sm text-zinc-500 hidden md:block">
+                        <time :datetime="date">{{ date }}</time>
+                    </p>
+                </div>
             </div>
-        </div>
+        </nuxt-link>
     </article>
 </template>
 
@@ -28,9 +30,16 @@ defineProps({
         required: true,
     },
     index: {
-        type: Object,
+        type: Number,
+        required: true
+    },
+    link: {
+        type: String,
         required: true
     }
 })
-
+function chineseNumbers(num) {
+    const chineseDigits = ['壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖', '拾'];
+    return chineseDigits[num];
+}
 </script>

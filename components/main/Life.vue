33680f5c023641a24dda940926508371853d6bb8
Qujia <duqujia@hotmail.com>
2024-12-12 17:51:36 +0800

    perf: Optimized data fetching by query configuration

--- components/main/Life.vue
+++ components/main/Life.vue
@@ -1,10 +1,14 @@
 <script setup>
-const { data } = await useAsyncData('blog', () => queryContent('/blog').limit(3).sort({ date: - 1 }).find(),)
+const query = {
+    path: '/blog',
+    limit: 3,
+    sort: { date: -1 },
+}
 </script>
 <template>
-    <ContentList path="/blog">
+    <ContentList :query="query" path="/blog">
         <template #default="{ list }">
-            <GlassCard v-for="(blog, index) in data" :key="blog._path" :title="blog.title"
+            <GlassCard v-for="(blog, index) in list" :key="blog._path" :title="blog.title"
                 :description="blog.description" :date="blog.date" :index="index" :link="blog._path">
             </GlassCard>
         </template>
--- components/main/Projects.vue
+++ components/main/Projects.vue
@@ -1,11 +1,15 @@
 <script setup>
-const { data } = await useAsyncData('projects', () => queryContent('/projects').limit(2).sort({ date: - 1 }).find(),)
+const query = {
+    path: '/projects',
+    limit: 2,
+    sort: { date: -1 },
+}
 </script>
 
 <template>
-    <ContentList path="/projects">
+    <ContentList :query="query" path="/projects">
         <template #default="{ list }">
-            <ProjectCard v-for="project in data" :key="project._path" :imgUrl="project.image" :title="project.title"
+            <ProjectCard v-for="project in list" :key="project._path" :imgUrl="project.image" :title="project.title"
                 :description="project.description" :year='project.date' :tags="project.tags" :link="project._path">
             </ProjectCard>
         </template>

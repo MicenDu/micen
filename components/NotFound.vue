520bb76400c373772131c52b8775e833359263a6
Micenjia <duqujia@hotmail.com>
2024-12-21 22:27:27 +0800

    feat: Added NotFound component

---
+++ components/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="text-center glass-border py-8">
+        <h1 class="text-zinc-700 text-4xl dark:text-zinc-300">
+            <slot></slot>
+        </h1>
+    </div>
+</template>

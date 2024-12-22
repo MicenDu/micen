b9744292b68bfba49dae625f39af96165d55816e
Micenjia <duqujia@hotmail.com>
2024-12-22 21:32:17 +0800

    perf: Replace img with NuxtImg for better image optimization

--- components/ToolCard.vue
+++ components/ToolCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full h-auto rounded-sm hover:bg-zinc-200 dark:hover:bg-zinc-600">
         <a :href="toolLink" class="flex flex-row items-center justify-center gap-4 my-3 mx-4 " target="_blank">
-            <img :src="iconPath" :alt="toolName" class="w-10 h-10">
+            <NuxtImg :src="iconPath" :alt="toolName" class="w-10 h-10" />
             <div class="flex flex-col justify-center  gap-0.5 w-full">
                 <p class="font-medium text-sm text-neutral-700 dark:text-zinc-300">
                     {{ toolName }}

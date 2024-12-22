f1b51209b28087aca3b73adff8236f53913b1c48
Micenjia <duqujia@hotmail.com>
2024-12-22 16:12:05 +0800

    style: Set min height for container to 85vh

--- layouts/center.vue
+++ layouts/center.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="container mx-auto pt-8">
+    <section class="container mx-auto pt-8 min-h-[85vh]">
         <div class="m-5 md:mx-20 md:my-16">
             <slot />
         </div>

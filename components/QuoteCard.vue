bb44fc484efbaea1f28a3ed4fb7b7b7ec2297544
Qujia <duqujia@hotmail.com>
2025-01-07 10:22:26 +0800

    fix: Remove extra p tag

--- components/QuoteCard.vue
+++ components/QuoteCard.vue
@@ -2,11 +2,9 @@
     <div class="p-4 my-4 glass-border flex flex-col justify-center gap-2 flex-shrink-0 
                 min-h-44 w-[90vw] max-w-3xl mx-auto
                 md:min-h-52 md:h-80 md:p-8">
-        <p class="mt-8">
-            <p class="text-3xl font-bold text-zinc-700 line-clamp-3 mb-3 w-full text-center font-serif md:text-4xl dark:text-zinc-200">
+            <p class="text-3xl font-bold text-zinc-700 line-clamp-3 mb-3 mt-8 w-full text-center font-serif md:text-4xl dark:text-zinc-200">
                 “{{quote}}”
             </p>
-        </p>
         <div class="flex justify-end">
             <cite class="text-zinc-400 font-serif">
                 —— {{cite}}

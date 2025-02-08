81ff4b2b3820fb19199995b495edee925a851b6f
Qujia <duqujia@hotmail.com>
2025-02-08 15:44:26 +0800

    fix: Replaced NuxtImg with img to resolve image issues on Netlify

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -3,7 +3,7 @@
         <slot name="default">
             <div class="">
                 <a :href="link">
-                    <NuxtImg :src="imgUrl" :alt="alt" format="avif, webp"
+                    <Img :src="imgUrl" :alt="alt" format="avif, webp"
                         class="object-cover object-center h-40 xl:h-64 w-full box-border" />
                     <div class="glass-border w-full">
                         <div class="flex justify-between p-4">

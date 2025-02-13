66c85166610a13e53915d7a2024ec913ca121e7f
Micenjia <duqujia@hotmail.com>
2025-02-13 22:22:54 +0800

    docs: Changed copyright year to 2025

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -15,7 +15,7 @@ const navigation = [
     <footer class="glass bottom-0 w-full mt-20">
         <div class="flex flex-row container justify-between relative py-6 px-8 md:px-0 md:mx-auto">
             <div class="flex text-sm text-gray-500 flex-col gap-4 md:flex-row md:gap-8 dark:text-zinc-300">
-                <span class="font-medium">Micen © 2024</span>
+                <span class="font-medium">Micen © 2025</span>
                 <div class="flex gap-6 ">
                     <a v-for="item in navigation" :key="item.name" :href="item.href">{{ item.name }}</a>
                 </div>
@@ -27,11 +27,11 @@ const navigation = [
                 <a href="https://www.linkedin.com/in/qujiadu/">
                     <ph-linkedin-logo-bold class="icons" />
                 </a>
-                <a href="https://www.instagram.com/micenjia/" class="icons">
-                    <mdi-instagram />
+                <a href="https://www.instagram.com/micenjia/">
+                    <mdi-instagram  class="icons"/>
                 </a>
-                <a href="mailto:duqujia@hotmail.com" class="icons">
-                    <i-custom-mail />
+                <a href="mailto:duqujia@hotmail.com" >
+                    <i-custom-mail class="icons"/>
                 </a>
             </div>
         </div>

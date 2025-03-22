aff02971e4de4412801edb3bc50018b1df33935f
Micenjia <duqujia@hotmail.com>
2025-03-22 22:42:55 +0800

    style: Adjusted picture height and width

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -3,9 +3,9 @@
         <slot name="default">
             <div class="">
                 <a :href="link">
-                    <NuxtPicture :src="imgUrl" :alt="alt" format="avif, webp"
-                        class="object-cover object-center h-40 xl:h-64 w-full box-border" />
-                    <div class="glass-border w-full">
+                    <NuxtPicture :src="imgUrl" :alt="alt" format="avif" class="h-40 xl:h-64 w-full overflow-hidden"
+                        :imgAttrs="{ style: 'object-fit:cover; object-position:center; height:16rem; width:-webkit-fill-available' }" />
+                    <div class=" glass-border w-full">
                         <div class="flex justify-between p-4">
                             <h2 class="text-zinc-500 dark:text-zinc-200">
                                 {{ title }}
